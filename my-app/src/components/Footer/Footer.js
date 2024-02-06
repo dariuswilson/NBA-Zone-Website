4a983d2a888f1c6ff9c1aa48005041c723585acd
Darius Wilson <dariuswilsonxu@gmail.com>
2024-02-06 15:23:54 -0600

    Finished footer, draft for main page done

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -1,60 +1,34 @@
-// Footer.js
 import React from "react";
 import "./Footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTiktok,
-  faInstagram,
-  faTwitter,
-  faDiscord,
-} from "@fortawesome/free-brands-svg-icons";
+import logo from "./nbazone.png";
+import discordIcon from "./discordlogo.png";
 
 function Footer() {
   return (
-    <footer className="site-footer">
-      <div className="footer-content">
-        <div className="footer-nav">
-          <a href="/">Home</a>
-          <a href="https://discord.gg/nbachat">Discord</a>
-          <a href="/">Edit</a>
-          {/* More links as needed */}
-        </div>
-        <div className="footer-contact">
-          <p>Contact us at: nbazonediscord@gmail.com</p>
-        </div>
-        <div className="footer-socials">
-          <a
-            href="https://www.tiktok.com/@nbatrends_"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faTiktok} size="2x" />
-          </a>
-          <a
-            href="https://www.instagram.com/swishundisputed/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </a>
-          <a
-            href="https://twitter.com/nbaTrends_"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faTwitter} size="2x" />
-          </a>
-          <a
-            href="https://discord.gg/nbachat"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faDiscord} size="2x" />
-          </a>
-        </div>
-        <div className="footer-legal">
-          <p>© 2024 NBA Zone Discord. All rights reserved.</p>
-        </div>
+    <footer className="footer">
+      <div className="logo-container">
+        <img src={logo} alt="NBA Zone" className="logo" />
+      </div>
+      <nav className="menu">
+        <a href="/nba-stream">
+          <span>NBA STREAM</span>
+        </a>
+        <a href="/">
+          <span>HOME</span>
+        </a>
+        <a href="/game-nights">
+          <span>GAME NIGHTS</span>
+        </a>
+        <a href="/nba-scores">
+          <span>NBA SCORES</span>
+        </a>
+      </nav>
+      <a href="https://discord.gg/nbazone" className="join-discord">
+        <img src={discordIcon} alt="Join Discord" />
+        <span>JOIN DISCORD</span>
+      </a>
+      <div className="footer-legal">
+        <p>© 2024 NBA Zone Discord. All rights reserved.</p>
       </div>
     </footer>
   );
