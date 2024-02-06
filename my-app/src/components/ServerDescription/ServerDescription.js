a7b8036673bc7eed091f77ecf151fe8572a4d33e
Darius Wilson <dariuswilsonxu@gmail.com>
2024-02-05 23:18:00 -0600

    Remodified Server Description

--- my-app/src/components/ServerDescription/ServerDescription.js
+++ my-app/src/components/ServerDescription/ServerDescription.js
@@ -1,43 +1,32 @@
-import React, { useState, useEffect } from "react";
+// ServerDescription.js
+import React from "react";
 import "./ServerDescription.css";
+import basketballImage from "./nba court.jpg"; // Update with the path to your image
+import discordIcon from "../Header/discordlogo.png"; // Make sure the path is correct
 
 function ServerDescription() {
-  const [activeCard, setActiveCard] = useState("memberCount");
-  const [fadeOut, setFadeOut] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setFadeOut(true);
-
-      setTimeout(() => {
-        setActiveCard((prevActiveCard) =>
-          prevActiveCard === "memberCount" ? "messageToday" : "memberCount"
-        );
-        setFadeOut(false);
-      }, 1000); //fadeOut timeout
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div className="server-description">
-      <h2>Not your typical NBA Discord Server</h2>
-      <div className="stats-container">
-        {activeCard === "memberCount" && (
-          <div className={`stat-card ${!fadeOut ? "fade-in" : "fade-out"}`}>
-            <h3>Server Member Count:</h3>
-            <p>24,000+</p>
-          </div>
-        )}
-        {activeCard === "messageToday" && (
-          <div className={`stat-card ${!fadeOut ? "fade-in" : "fade-out"}`}>
-            <h3>Message Today:</h3>
-            <p>8,000+</p>
-          </div>
-        )}
+    <section className="server-description">
+      <div className="text-content">
+        <h2>24,000+ Members</h2>
+        <p>
+          NBA Zone is a NBA forum with currently 24,000+ members and counting.
+          The NBA discord server is super active and you can join the server for
+          free! NBA forum gives you access to live streams, game nights,
+          merchandise, and ability to chat with NBA fans around the world 24x7.
+          We are an inclusive community and provide basketball fans an awesome
+          place to hangout.
+        </p>
+        <a href="https://discord.gg/nbazone" className="join-discord-button">
+          <img src={discordIcon} alt="Discord" className="discord-logo" /> JOIN
+          24,000+ MEMBERS ON DISCORD
+        </a>
       </div>
-    </div>
+      <div className="image-content">
+        <img src={basketballImage} alt="Basketball Action" />
+        <blockquote>"Alive, Active, & Ever Growing Community!"</blockquote>
+      </div>
+    </section>
   );
 }
 
